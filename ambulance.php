1db7cf4cfc079ed8e3de5c748b0cb4baf3f60469
Dipannita <dipannitachowdhury32023@gmail.com>
2026-09-13 00:22:28 +0600

    Fix database name in connection string

--- ambulance.php
+++ ambulance.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "care_and_cure");
+$conn = mysqli_connect("localhost", "root", "", "care and cure");
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
